b495369944e9eed322e8d499cb2eb32df428b98e
Junior Santos Oliveira Mariano <junior_santos2100@outlook.com>
2022-09-17 12:37:37 -0300

    Exemplo de crud utilizando arquivo json

--- index.js
+++ index.js
@@ -1,25 +1,78 @@
+const fileSystem = require("fs")
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello world')
+const filePath = './database/database.json'
+
+app.use(express.json())
+
+function readFile() {
+    return JSON.parse(fileSystem.readFileSync(filePath))
+}
+
+app.get('/usuarios', function (req, res) {
+    const usuariosCadastrados = readFile()
+
+    res.json(usuariosCadastrados)
 })
 
-app.post("/", function (req, res) {
-    res.send("Hello world com Post")
+app.post("/usuarios", function (req, res) {
+    const usuariosCadastrados = readFile()
+
+    const idAtual = usuariosCadastrados
+        .reduce((previous, current) => {
+            if (!current) return 1
+
+            if (previous.id > current.id) return previous.id
+
+            return current.id
+        }, 0)
+
+    const usuarioAtual = {
+        ...req.body,
+        id: idAtual + 1
+    }
+
+    usuariosCadastrados.push(usuarioAtual)
+
+    fileSystem.writeFileSync(filePath, JSON.stringify(usuariosCadastrados))
+
+    res.status(201).json(usuarioAtual)
 })
 
-app.put("/", function (req, res) {
-    res.send("Hello world com Put")
+app.put("/usuarios/:usuarioId", function (req, res) {
+    const usuariosCadastrados = readFile()
+
+    const usuarioIds = usuariosCadastrados.map(usuario => usuario.id)
+    const usuarioParaAtualizarIndice = usuarioIds.indexOf(Number(req.params.usuarioId))
+
+    usuariosCadastrados[usuarioParaAtualizarIndice] = {
+        ...req.body,
+        id: usuariosCadastrados[usuarioParaAtualizarIndice].id
+    }
+
+    fileSystem.writeFileSync(filePath, JSON.stringify(usuariosCadastrados))
+
+    res.json(usuariosCadastrados[usuarioParaAtualizarIndice])
 })
 
-app.get("/:meuParametro", function (req, res) {
-    res.send(`Hello world com ${req.params.meuParametro}`)
+app.get("/usuarios/:usuarioId", function (req, res) {
+    const usuariosCadastrados = readFile()
+
+    const [usuario] = usuariosCadastrados.filter(usuarioCadastrado => usuarioCadastrado.id === Number(usuarioId))
+
+    res.json(usuario)
 })
 
-app.delete("/", function (req, res) {
-    res.send("Hello world com delete")
+app.delete("/usuarios/:usuarioId", function (req, res) {
+    const usuariosCadastrados = readFile()
+
+    const usuariosAtualizados = usuariosCadastrados.filter(usuarioCadastrado => usuarioCadastrado.id !== Number(req.params.usuarioId))
+
+    fileSystem.writeFileSync(filePath, JSON.stringify(usuariosAtualizados))
+
+    res.sendStatus(204)
 })
 
 app.listen(port)
